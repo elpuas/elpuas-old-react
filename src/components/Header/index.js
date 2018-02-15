5cf5e31beccd64332491db46816849eb173824cc
el Puas <elpuas@gmail.com>
2018-02-14 19:07:31 -0500

    add hamburger menu states

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { slide as Menu } from 'react-burger-menu'
-import Nav from './navigation'
+import { Link } from 'react-router-dom';
 
 import FontAwesome from 'react-fontawesome'
 import './styles.scss'
@@ -8,13 +8,38 @@ import './styles.scss'
 let BRAND = "el.puas";
 
 export default class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      menuOpen: false
+    }
+  }
+    handleStateChange(state) {
+    this.setState({menuOpen: state.isOpen})
+  }
+  closeMenu() {
+    this.setState({menuOpen: false})
+  }
+  toggleMenu() {
+    this.setState({menuOpen: !this.state.menuOpen})
+  }
   render() { // Why the props work if calling like this?
       return (
       <header>
         <h1 className="header__logo">{BRAND}</h1>
-        <Menu right bodyClassName={ "header__nav--container" }>
+        <Menu
+        right
+        bodyClassName={ "header__nav--container" }
+        isOpen={this.state.menuOpen}
+        onStateChange={(state) => this.handleStateChange(state)}
+        >
           <main>
-            <Nav />
+            <ul>
+              <li><Link to="/" onClick={() => this.closeMenu()}>Home</Link></li>
+              <li><Link to="/profile" onClick={() => this.closeMenu()}>Profile</Link></li>
+              <li><Link to="/works" onClick={() => this.closeMenu()}>Works</Link></li>
+              <li><Link to="/blog" onClick={() => this.closeMenu()}>Blog</Link></li>
+            </ul>
             <div className="header__contact">
                 <h2>Contact:</h2>
                 <ul>
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const Profile = () => (
-  <div>
-    <h1>Welcome to my Profile Page</h1>
-  </div>
-)
-
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appClass: 'myClass'
+    };
+  }
+  componentDidMount() {
+    this.setState({ appClass: 'newClass' });
+  }
+  render() {
+    return (
+      <div className={`DefaultClass ${this.state.appClass}`}>
+        <h1>Welcome to my Profile Page</h1>
+        <button className="square" onClick={() => console.log("Hice Click")}>
+      </button>
+      </div>
+    )
+  }
+}
 export default Profile
